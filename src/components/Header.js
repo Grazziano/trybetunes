f2e52809eb225039bbc96259bc8fc9a60808dbdf
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-11-15 11:01:05 -0300

    Requisito 3 : Crie um componente de cabeçalho

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    getUser().then(({ name }) => this.setState({ name, loading: false }));
+  }
+
   render() {
+    const { name, loading } = this.state;
+
+    if (loading) return <Loading />;
+
     return (
-      <header data-testid="header-component" />
+      <header data-testid="header-component">
+        <span data-testid="header-user-name">{name}</span>
+      </header>
     );
   }
 }
